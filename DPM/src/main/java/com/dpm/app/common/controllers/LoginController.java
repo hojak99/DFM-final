0789e2b4d8e03cd1181c741e940f71ea37489bbd
KwonJH <hojak99@gmail.com>
2018-01-18 11:10:46 +0900

    로그인 컨트롤러 추가

---
+++ DPM/src/main/java/com/dpm/app/common/controllers/LoginController.java
@@ -0,0 +1,24 @@
+package com.dpm.app.common.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+@Controller
+public class LoginController {
+	
+	@ResponseBody
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(HttpServletRequest request, HttpServletResponse response, @RequestParam("param") String param) throws JsonProcessingException {
+		
+		return "";
+	}
+}
