a62ff14ccd2059854db697102a506b49c994d7ce
KwonJH <hojak99@gmail.com>
2018-01-27 12:44:23 +0900

    todo dto 추가

---
+++ DPM/src/main/java/com/dpm/app/todo/dto/TodoDto.java
@@ -0,0 +1,66 @@
+package com.dpm.app.todo.dto;
+
+import java.util.Date;
+
+public class TodoDto {
+	private int id;
+	private String tag;
+	private int finish;
+	private String title;
+	private String user_id;
+	private Date create_date;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public int getFinish() {
+		return finish;
+	}
+
+	public void setFinish(int finish) {
+		this.finish = finish;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public Date getCreate_date() {
+		return create_date;
+	}
+
+	public void setCreate_date(Date create_date) {
+		this.create_date = create_date;
+	}
+
+	@Override
+	public String toString() {
+		return "TodoDto [id=" + id + ", tag=" + tag + ", finish=" + finish + ", title=" + title + ", user_id=" + user_id
+				+ ", create_date=" + create_date + "]";
+	}
+}
