ddfdb58bc987ea87301d1f8be0113be492b23e61
KwonJH <hojak99@gmail.com>
2018-01-27 12:20:53 +0900

    테이블 추가

--- DPM/sql/query.sql
+++ DPM/sql/query.sql
@@ -5,6 +5,29 @@ create table user (
     password VARCHAR(50) not null
 );
 
+create table token (
+	token_key int not null auto_increment primary key,
+    token_value int not null
+);
+
+
+create table diary (
+	id int not null auto_increment primary key,
+    title varchar(50) not null,
+    content varchar(200) not null,
+    create_date datetime not null,
+    user_id varchar(50) not null
+);
+
+create table todo(
+	id int not null auto_increment primary key,
+    tag varchar(100),
+    finish int not null,
+    title varchar(50) not null,
+    create_date datetime not null,
+    user_id varchar(50) not null
+);
+
 create table moneybook (
 	id int(11) unsigned NOT NULL auto_increment,
 	date date,
@@ -13,6 +36,16 @@ create table moneybook (
     isCash TINYINT(1),
     isExpense TINYINT(1),
 	PRIMARY KEY(id)
+);
+
+create table schedule (
+	id varchar(50) primary key,
+    user_id varchar(50),
+    promise_time int not null,
+    title varchar(50) not null,
+    location varchar(50),
+    finish int not null,
+    create_date datetime default current_timestamp
 );
 
 insert into user values('jinhyeon', '1234');
