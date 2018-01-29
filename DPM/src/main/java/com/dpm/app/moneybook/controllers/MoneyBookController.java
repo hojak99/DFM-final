7d890b48df66059b8d11baf2e54ba42f902a6f85
Jinhyeon Park <wlsgussla123@gmail.com>
2018-01-29 15:24:50 +0900

    ResponseStatus code

--- DPM/src/main/java/com/dpm/app/moneybook/controllers/MoneyBookController.java
+++ DPM/src/main/java/com/dpm/app/moneybook/controllers/MoneyBookController.java
@@ -1,20 +1,20 @@
 package com.dpm.app.moneybook.controllers;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.dpm.app.moneybook.dto.MoneyBookDTO;
 import com.dpm.app.moneybook.services.MoneyBookService;
@@ -27,12 +27,14 @@ public class MoneyBookController {
 	private MoneyBookService moneyBookService;
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
 	public @ResponseBody List<MoneyBookDTO> getAllMoneyBooks() {
 		logger.info("MONEYBOOK CONTROLLER IN=>>");
 		return moneyBookService.getAllMoneyBooks();
 	}
 
 	@RequestMapping(value = "/{dateString}", method = RequestMethod.GET)
+	@ResponseStatus(HttpStatus.OK)
 	public @ResponseBody List<MoneyBookDTO> getMoneyBook(
 			@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) String dateString) throws ParseException {
 		logger.info("MONEYBOOK CONTROLLER IN=>>");
@@ -40,6 +42,7 @@ public class MoneyBookController {
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.OK)
 	public void modifyMoneyBook(@RequestBody final MoneyBookDTO moneyBook) {
 		logger.info("MONEYBOOK CONTROLLER IN=>>");
 		System.out.println(moneyBook);
@@ -47,18 +50,21 @@ public class MoneyBookController {
 	}
 
 	@RequestMapping(value = "", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
 	public void deleteAllMoneyBooks() {
 		logger.info("MONEYBOOK CONTROLLER IN=>>");
 		moneyBookService.deleteAllMoneyBooks();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
 	public void deleteMoneyBook(@PathVariable int id) {
 		logger.info("MONEYBOOK CONTROLLER IN=>>");
 		moneyBookService.deleteMoneyBook(id);
 	}
 	
 	@RequestMapping(value = "/date/{dateString}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
 	public void deleteMoneyBookByDate(
 			@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) String dateString) {
 		logger.info("MONEYBOOK CONTROLLER IN=>>");
