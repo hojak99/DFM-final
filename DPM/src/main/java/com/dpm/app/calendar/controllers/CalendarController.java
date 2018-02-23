038699b38fc4e90f3809db34ed9a5fb41ff1d859
KwonJH <hojak99@gmail.com>
2018-02-23 17:44:42 +0900

    캘린더 컨트롤러 추가

--- DPM/src/main/java/com/dpm/app/calendar/controllers/CalendarController.java
+++ DPM/src/main/java/com/dpm/app/calendar/controllers/CalendarController.java
@@ -1,6 +1,10 @@
 package com.dpm.app.calendar.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,9 +20,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.dpm.app.diary.dto.DiaryDto;
 import com.dpm.app.diary.dto.DiarySearchDto;
 import com.dpm.app.diary.services.DiaryService;
+import com.dpm.app.todo.dto.TodoDto;
+import com.dpm.app.todo.dto.TodoSearchDto;
+import com.dpm.app.todo.services.TodoService;
 
 /**
- * Ķ���� ���� Controller
  * @author KwonJH
  *
  */
@@ -30,13 +36,58 @@ public class CalendarController {
 	@Autowired
 	DiaryService diaryService;
 	
+	@Autowired
+	TodoService todoService;
+	
 	@ResponseBody
 	@RequestMapping(value="/calendar", method=RequestMethod.GET)
-	public String calendarHome(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	public Map<String, Object> calendarHome(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		DiarySearchDto searchDto = new DiarySearchDto();
-		searchDto.setUser_id("test");
-		List<DiaryDto> diaryList = diaryService.getAllDiary(searchDto);
+		Date date = new Date();
+		SimpleDateFormat ymdFormat = new SimpleDateFormat("yyyyMMdd");
+		SimpleDateFormat ymFormat = new SimpleDateFormat("yyyyMM");
+		SimpleDateFormat yFormat = new SimpleDateFormat("yyyy");
+		
+		Map<String, Object> jsonObject = new HashMap<String, Object>();
+		
+		TodoSearchDto todoSearchDto = new TodoSearchDto();
+		todoSearchDto.setUser_id("test");
+		
+		DiarySearchDto diarySearchDto = new DiarySearchDto();
+		diarySearchDto.setUser_id("hojak99");
+	
+		Map<String, Object> jsonTodo = new HashMap<>();
+		Map<String, Object> jsonDiary = new HashMap<>();
+		for(int i = 1; i<13; ++i) {
+			if(i < 10) {
+				diarySearchDto.setCreate_date_start(yFormat.format(date)+"0"+i);
+				todoSearchDto.setCreate_date_start(yFormat.format(date)+"0"+i);
+			} else {
+				diarySearchDto.setCreate_date_start(yFormat.format(date)+i);
+				todoSearchDto.setCreate_date_start(yFormat.format(date)+i);
+			}
+			
+			List<TodoDto> todoList = todoService.getTodo(todoSearchDto);
+			List<DiaryDto> diaryList = diaryService.getDiary(diarySearchDto);
+			
+			if(todoList != null || todoList.size() == 0) {
+				jsonDiary.put(String.valueOf(i), diaryList.toString());	
+			}else {
+				jsonDiary.put(String.valueOf(i), "null");
+			}
+			
+			if(diaryList != null || diaryList.size() == 0){
+				jsonTodo.put(String.valueOf(i), todoList.toString());	
+			} else {
+				jsonTodo.put(String.valueOf(i), "null");
+			}
+			
+			
+		}
+		
+		jsonObject.put("diary", jsonDiary);
+		
+		
 		
 		return "HOME";
 	}
--- DPM/src/main/java/com/dpm/app/todo/dto/TodoSearchDto.java
+++ DPM/src/main/java/com/dpm/app/todo/dto/TodoSearchDto.java
@@ -4,7 +4,9 @@ import java.util.Date;
 
 public class TodoSearchDto {
 	private String user_id;
-	private String create_date;
+	private String create_date;				// 기본 20181231이 들어감 (생성, 삭제할 시 사용)
+	private String create_date_start;		// 조회할 때 12월을 조회할 시 201812 값이 들어감
+	private String create_date_end; 		// 조회할 때 12월을 조회할 시 20181231 값이 들어감
 	private int id;
 
 	public String getUser_id() {
@@ -31,9 +33,28 @@ public class TodoSearchDto {
 		this.id = id;
 	}
 
+	public String getCreate_date_start() {
+		return create_date_start;
+	}
+
+	public void setCreate_date_start(String create_date_start) {
+		this.create_date_start = create_date_start;
+	}
+
+	public String getCreate_date_end() {
+		return create_date_end;
+	}
+
+	public void setCreate_date_end(String create_date_end) {
+		this.create_date_end = create_date_end;
+	}
+
 	@Override
 	public String toString() {
-		return "TodoSearchDto [user_id=" + user_id + ", create_date=" + create_date + ", id=" + id + "]";
+		return "TodoSearchDto [user_id=" + user_id + ", create_date=" + create_date + ", create_date_start="
+				+ create_date_start + ", create_date_end=" + create_date_end + ", id=" + id + "]";
 	}
+
+	
 
 }
